ab5d8df6210de074d45f5949f73c9e7818f8191b
gelmez <gelmezdervis@gmail.com>
2025-04-27 21:37:24 +0300

    update - fixture apis and catch exceptions

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,6 +1,15 @@
 <?php
 
+use App\Http\Controllers\FixtureController;
+use App\Http\Controllers\FixtureGroupController;
+use App\Http\Controllers\FixtureMatchesController;
 use App\Http\Controllers\TeamController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('/teams', TeamController::class);
+Route::apiResource('/teams', TeamController::class)->only(['index', 'show']);
+Route::apiResource('/fixtures', FixtureController::class)->only(['index', 'store', 'destroy']);
+
+Route::prefix('/fixtures/{fixture}')->group(function () {
+    Route::get('/groups', [FixtureGroupController::class, 'index'])->name('fixtures.groups.list');
+    Route::get('/matches', [FixtureMatchesController::class, 'index'])->name('fixtures.groups.list');
+});
