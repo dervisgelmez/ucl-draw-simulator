4929cd7d238792bb25f61c0af02ca16c01aab21f
gelmez <gelmezdervis@gmail.com>
2025-04-28 03:05:17 +0300

    fix - stage enum order type

--- api/app/Models/Stage.php
+++ api/app/Models/Stage.php
@@ -6,8 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Stage extends Model
 {
+    protected $guarded = [];
+
     public static function findFirstStageId()
     {
         return Stage::query()->orderBy('order')->first()->id;
+    }
+
+    public static function findNextStageId(Stage $stage)
+    {
+        return Stage::query()->where('order', ($stage->order + 1))->first()->id;
     }
 }
