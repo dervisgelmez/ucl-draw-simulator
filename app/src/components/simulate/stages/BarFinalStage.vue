bbe002e7ff62b4bbb6449dea0eb87be4d5465b60
gelmez <gelmezdervis@gmail.com>
2025-04-29 02:43:41 +0300

    fix - simulate button disable after the completed final

--- app/src/components/simulate/stages/BarFinalStage.vue
+++ app/src/components/simulate/stages/BarFinalStage.vue
@@ -1,5 +1,6 @@
 <template>
   <div
+    v-if="!finalMatch.completedAt"
     @click="simulateFinal"
     type="button"
     class="flex items-center gap-1 text-indigo-600 cursor-pointer text-base border-gray-200 pr-3"
@@ -13,12 +14,23 @@
 import { Icon } from '@iconify/vue'
 import { useApiRequest } from '@/composables/useApiRequest.ts'
 import { useFixtureStore } from '@/stores/fixture.store.ts'
-import { FixtureStages, type IFixtureStageComponentsProps } from '@/types/fixture.ts'
+import {
+  type FixtureMatch,
+  FixtureStages,
+  type IFixtureStageComponentsProps
+} from '@/types/fixture.ts'
+import { storeToRefs } from 'pinia'
+import { computed, type Ref } from 'vue'
 
 const props = defineProps<IFixtureStageComponentsProps>()
 
 const { request } = useApiRequest()
 const { resetFixtureMatch, fetchFixtureMatches } = useFixtureStore()
+const { fixtureMatches } = storeToRefs(useFixtureStore())
+
+const finalMatch: Ref<FixtureMatch> = computed(() => {
+  return fixtureMatches.value[FixtureStages.FINAL][0]
+})
 
 const applySimulate = async () => {
   resetFixtureMatch(FixtureStages.FINAL)
